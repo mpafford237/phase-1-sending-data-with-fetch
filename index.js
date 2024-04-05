630bb22ee59dfee9f680ebfa36444ae4b4c5db62
masonPafford <mdpafford2015@gmail.com>
2024-04-05 17:26:22 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        document.getElementById("userId").innerHTML = `User ID: ${data.id}`
+    })
+    .catch((error) => {
+        alert("Bad! Bad! Bad!")
+        document.getElementById("userId").innerHTML = error.message
+    })
+}
